20fba59f77bd8a4ec2e51a26799c9611c8b02a61
mikeAdams <mikelovesbooks@gmail.com>
2018-03-19 16:03:45 +0000

    Changed database name to 'rooms'

--- InitDatabase.sh
+++ InitDatabase.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-mongo mongodb://localhost:27017/locations <<EOF
+mongo mongodb://localhost:27017/rooms <<EOF
 db.dropDatabase();
 
-db.locations.insert({
-      ",ocationName": "StartRoom",
-      "locationDesc": " A small dingy room. The wet floor shallowly slopes to a " +
+db.rooms.insert({
+      "roomName": "StartRoom",
+      "roomDesc": " A small dingy room. The wet floor shallowly slopes to a " +
       "small grill in the center of the floor. Dim light comes from a single flickering bulb hanging from the ceiling " +
       "that fitfully illuminates a single heavy wooden door in the north wall." +
       "" +
       "The room smells of damp and neglect and something you can't place, something.....wrong.",
-      "locationImage": ""
+      "roomImage": ""
 });
 
 EOF
